eb9fc2277b8fa5e7e7b223d3ae5797a59fdfc25b
Sean Murphy <sdmurphy.1999@gmail.com>
2022-04-23 14:02:14 -0400

    Adds auto layout constraints for portrait and landscape

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clearButtonPushed(_ sender: UIButton) {
+    }
+    
 }
 
